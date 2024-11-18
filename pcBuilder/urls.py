aafb2a63dd0321ec493600cdb8ca6c2a6bb0029b
ilchii <ilchiibohdan@gmail.com>
2024-11-18 15:18:49 +0200

    improved mainapp

--- pcBuilder/urls.py
+++ pcBuilder/urls.py
@@ -15,8 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.views.static import serve
+from django.urls import path, include, re_path
+from filebrowser.sites import site
+from django.conf.urls.static import static
+
+from pcBuilder import settings
 
 urlpatterns = [
+    path('admin/filebrowser/', site.urls),
     path('admin/', admin.site.urls),
-]
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    path("", include("mainapp.urls")),
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
