fda24712292081d1021b29fad918855e1b659d7c
ilchii <ilchiibohdan@gmail.com>
2024-12-12 11:46:33 +0200

    developed login

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path, include, re_path
-from mainapp.views import index
+from django.contrib.auth import views as auth_views
+from mainapp.views import index, signup
 
 urlpatterns = [
     path('', index, name="index"),
+    path('login/', auth_views.LoginView.as_view(template_name='mainapp/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('signup/', signup, name="signup")
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
 def index(request):
-
     return render(request, 'mainapp/index.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')  # Redirect to login after signup
+    else:
+        form = UserCreationForm()
+    return render(request, 'mainapp/signup.html', {'form': form})
