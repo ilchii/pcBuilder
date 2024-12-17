a75de67f13c962a981a6230a989301eaa5589281
ilchii <ilchiibohdan@gmail.com>
2024-12-17 14:26:24 +0200

    Base version of the Builder form

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include, re_path
 from django.contrib.auth import views as auth_views
-from mainapp.views import index, signup, list
+from mainapp.views import index, signup, list, create_build
 
 urlpatterns = [
     path('', index, name="index"),
     path('login/', auth_views.LoginView.as_view(template_name='mainapp/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('signup/', signup, name="signup"),
-    path('list/', list, name="list")
+    path('list/', list, name="list"),
+    path("create-build/", create_build, name="create_build")
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
-from mainapp.models import Processor, Motherboard, Memory, Storage, VideoCard, Case, PowerSupply
+from django.contrib.auth.decorators import login_required
+from mainapp.models import Build, Processor, Motherboard, Memory, Storage, VideoCard, Case, PowerSupply
 
 # Create your views here.
 
@@ -28,3 +29,46 @@ def list(request):
         "powersupplies": PowerSupply.objects.all(),
     }
     return render(request, 'mainapp/list.html', context)
+
+@login_required
+def create_build(request):
+    if request.method == "POST":
+        # Get the user
+        user = request.user
+
+        # Get selected components
+        build_name = request.POST.get("build_name", "Untitled Build")
+        processor = Processor.objects.get(id=request.POST["processor"]) if request.POST.get("processor") else None
+        motherboard = Motherboard.objects.get(id=request.POST["motherboard"]) if request.POST.get("motherboard") else None
+        memory = Memory.objects.get(id=request.POST["memory"]) if request.POST.get("memory") else None
+        storage = Storage.objects.get(id=request.POST["storage"]) if request.POST.get("storage") else None
+        video_card = VideoCard.objects.get(id=request.POST["video_card"]) if request.POST.get("video_card") else None
+        case = Case.objects.get(id=request.POST["case"]) if request.POST.get("case") else None
+        power_supply = PowerSupply.objects.get(id=request.POST["power_supply"]) if request.POST.get("power_supply") else None
+
+        # Create the Build
+        Build.objects.create(
+            user=user,
+            name=build_name,
+            processor=processor,
+            motherboard=motherboard,
+            memory=memory,
+            storage=storage,
+            video_card=video_card,
+            case=case,
+            power_supply=power_supply,
+        )
+
+        return redirect("index")  # Redirect to a list of builds or another page after saving
+
+    # Load available components for selection
+    context = {
+        "processors": Processor.objects.all(),
+        "motherboards": Motherboard.objects.all(),
+        "memories": Memory.objects.all(),
+        "storages": Storage.objects.all(),
+        "videocards": VideoCard.objects.all(),
+        "cases": Case.objects.all(),
+        "powersupplies": PowerSupply.objects.all(),
+    }
+    return render(request, "mainapp/list.html", context)
